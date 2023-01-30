7b142441656512e67ab0690da71c22ecbff86d08
RanganaDinesh <118040042+RanganaDinesh@users.noreply.github.com>
2023-01-30 21:38:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// // Parent object
+// var Parent = function() {};
+
+// Parent.prototype.greet = function() {
+//   return "Hello from Parent";
+// };
+
+// Parent.prototype.sayGoodbye = function() {
+//   return "Goodbye from Parent";
+// };
+
+// // Child object
+// var child = function() {};
+
+// // Inherit methods from Parent
+// child.prototype = Object.create(Parent.prototype);
+
+// // Use parent methods in Child
+// var child = new child();
+// console.log(child.greet());
+// console.log(child.sayGoodbye());
